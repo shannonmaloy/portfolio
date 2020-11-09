be07f8c02e2acf477c5337dc42ff6ed8ccfd0d8c
Shannon Maloy <shannon.maloy@gmail.com>
2020-11-09 15:02:15 -0500

    Updated resume link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ export default class Home extends Component {
                             <div className="desc">
                           <h1>Welcome!</h1>
                           <h2>I'm Shannon Maloy - a successful Realtor with an education in Biological Engineering turned full-stack Software Engineer.  I am a problem solver by nature and thrive in the victory of achieving a goal.</h2>
-                            <p><a href="https://drive.google.com/file/d/1AbhMf0UJdIazsBVXtH2k9BA9gZSr9d3I/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-learn">View Resume <em className="icon-download4" /></a></p>
+                            <p><a href="https://drive.google.com/file/d/1XJXv1LaOjeyUlvnCIayBLKRDi05G3Tj3/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-learn">View Resume <em className="icon-download4" /></a></p>
                             </div>
                         </div>
                         </div>
